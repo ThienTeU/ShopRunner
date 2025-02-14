f7070163c79d99c55bca7453f6d510f1638fc45e
Duc Anh <anhtdhe186251@fpt.edu.vn>
2025-02-14 11:47:59 +0700

    comment ducanh

--- src/java/Controller/PostListController.java
+++ src/java/Controller/PostListController.java
@@ -1,9 +1,11 @@
+package Controller;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
-package controller;
+
 
 import DAO.PostCategoryDAO;
 import DAO.PostListDAO;
